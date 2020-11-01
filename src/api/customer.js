1d4f26c6ee0677d0d0dd06dfebacfce0187de26c
Thomas Joshi <thomas@asia-genomics.com>
2020-11-02 01:51:04 +1300

    switched to baseURL: 'http://3.26.48.152/selfcheckout'

--- src/api/customer.js
+++ src/api/customer.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 
-
+// baseURL: 'http://192.168.1.6/selfcheckout' => Local API Server 
+// baseURL: 'http://3.26.48.152/selfcheckout' => AWS API server
 export default axios.create({
-    baseURL: 'http://192.168.1.6/selfcheckout'
+    baseURL: 'http://3.26.48.152/selfcheckout'
 });
